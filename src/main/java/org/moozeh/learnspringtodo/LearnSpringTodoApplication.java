ae6738572257d456f25527849763771a84d8cb47
Chanwoo Kim <kcw2205@naver.com>
2025-02-24 11:54:56 +0900

    Initial Commit

---
+++ src/main/java/org/moozeh/learnspringtodo/LearnSpringTodoApplication.java
@@ -0,0 +1,13 @@
+package org.moozeh.learnspringtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringTodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/moozeh/learnspringtodo/LearnSpringTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.moozeh.learnspringtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
